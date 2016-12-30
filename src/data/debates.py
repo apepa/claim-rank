03c513054406724c23230e3b4efe075952c37a16
Pepa Gencheva <pepa.k.gencheva@gmail.com>
2016-12-30 18:02:44 +0200

    Fix, because enum is not iterable.

--- src/data/debates.py
+++ src/data/debates.py
@@ -15,10 +15,10 @@ class Debate(Enum):
     THIRD = 4
 
 
-def read_debates(debates, use_label = 'sum_all'):
+def read_debates(debate, use_label='sum_all'):
     """
     Reads the debate transcripts data.
-    :param debates: list of the debates(Debate enums) to return the sentences for.
+    :param debate: debates (Debate enum) to return the sentences for.
     :param use_label: how to form the gold label for the sentences
     - sum_all : label is the number of annotators that have agreed
     - lambda function : a custom function for a label, with input - the columns from file
@@ -26,26 +26,25 @@ def read_debates(debates, use_label = 'sum_all'):
 
     Examples:
     1. Take for claims only those that more than one annotator agrees on it
-    >>> read_debates([Debate.FIRST], lambda x: 1 if int(x[2])>1 else 0)
+    #>>> read_debates(Debate.FIRST, lambda x: 1 if int(x[2])>1 else 0)
     2. Take the number of annotators that have agreed on it
-    >>> read_debates([Debate.FIRST])
+    #>>> read_debates(Debate.FIRST)
     """
     sentences = []
-    for debate in debates:
-        debate_file_name = join(CONFIG['tr_all_anns'], CONFIG[debate.name] + FILE_EXT)
-        debate_file = open(debate_file_name)
-        debate_file.readline()
-        for line in debate_file:
-            line = line.strip()
-            columns = line.split(SEP)
+    debate_file_name = join(CONFIG['tr_all_anns'], CONFIG[debate.name] + FILE_EXT)
+    debate_file = open(debate_file_name)
+    debate_file.readline()
+    for line in debate_file:
+        line = line.strip()
+        columns = line.split(SEP)
 
-            if use_label == 'sum_all':
-                label = int(columns[2].strip())
-            else:
-                label = use_label(columns)
+        if use_label == 'sum_all':
+            label = int(columns[2].strip())
+        else:
+            label = use_label(columns)
 
-            s = Sentence(columns[0], columns[-1], label, columns[1], debate)
-            sentences.append(s)
+        s = Sentence(columns[0], columns[-1], label, columns[1], debate)
+        sentences.append(s)
 
     return sentences
 
