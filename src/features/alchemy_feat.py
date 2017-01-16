6e53467b0cf2d161103cc9e79db0e432deaffc94
Pepa Gencheva <pepa.k.gencheva@gmail.com>
2017-01-16 17:56:27 +0200

    Sentiment and NER features from Alchemy

---
+++ src/features/alchemy_feat.py
@@ -0,0 +1,49 @@
+from src.features.features import Feature
+from src.utils.config import get_config
+
+CONFIG = get_config()
+
+
+class NER(Feature):
+    """Adds Named Entities from Alchemy API"""
+    FEATS = ['ner']
+
+    def __init__(self):
+        types_file = open(CONFIG['alchemy_ner'])
+        self.ner = {}
+        columns = []
+        types_file.readline()
+        for line in types_file:
+            line = line.strip()
+            columns = line.split("\t")
+            self.ner[columns[0] + " " + columns[1]] = [int(v) for v in columns[2:]]
+        self.len = len(columns) - 2
+
+    def transform(self, X):
+        for sent in X:
+            sent.features['ner'] = self.ner[str(sent.id) + " " + sent.debate.name]
+        return X
+
+
+class Sentiment(Feature):
+    """Adds Sentiment score from Alchemy API."""
+    SENTIMENT_INDEX = 3
+    DEBATE_INDEX = 1
+    ID_INDEX = 0
+    FEATS = ['sent']
+
+    def __init__(self):
+        self.sentiment = {}
+
+        # read serialized sentiment scores
+        serialized_sentiment = open(CONFIG['sentiment_file'])
+        serialized_sentiment.readline()
+        for line in serialized_sentiment:
+            line = line.strip()
+            columns = line.split("\t")
+            self.sentiment[columns[self.DEBATE_INDEX] + columns[self.ID_INDEX]] = columns[self.SENTIMENT_INDEX]
+
+    def transform(self, X):
+        for sent in X:
+            sent.features['sent'] = self.sentiment[sent.debate.name + sent.id]
+        return X
