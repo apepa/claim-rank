84b5abd698e06cf12f45d4ae305ba9b75e1b2bc7
Pepa Gencheva <pepa.k.gencheva@gmail.com>
2017-05-24 10:22:46 +0300

    Fix - use the wright function for timing.

--- src/utils/timing.py
+++ src/utils/timing.py
@@ -6,9 +6,9 @@ from contextlib import contextmanager
 def timing(func):
     @functools.wraps(func)
     def newfunc(*args, **kwargs):
-        start_time = time.time()
+        start_time = time.clock()
         func(*args, **kwargs)
-        elapsed_time = time.time() - start_time
+        elapsed_time = time.clock() - start_time
         print('function [{}] finished in {} ms'.format(
             func.__name__, int(elapsed_time * 1000)))
     return newfunc
@@ -16,7 +16,7 @@ def timing(func):
 
 @contextmanager
 def timeit_context(name):
-    start_time = time.time()
+    start_time = time.clock()
     yield
-    elapsed_time = time.time() - start_time
+    elapsed_time = time.clock() - start_time
     print('[{}] finished in {} ms'.format(name, int(elapsed_time * 1000)))
