a986ba84a724795dca2b66e608ec4a8e4edd21b5
ijaradat <ijaradat@qf.org.qa>
2017-06-13 12:35:08 +0300

    sorting results functionality added

--- src/demoAPI.py
+++ src/demoAPI.py
@@ -3,11 +3,11 @@ import sys
 # for Arabic encoding purposes
 reload(sys)
 sys.setdefaultencoding('utf-8')
-import codecs
 from src.data.models import *
 from src.data.main import *
-from flask import Flask, render_template,request,redirect, url_for
+from flask import Flask,session, render_template,request,redirect, url_for,escape
 app = Flask(__name__)
+app.secret_key = 'IsraaQasim89'
 
 # if the button "showResults" is clicked , redirect the user to the results page carrying the textfield value in the request object
 @app.route("/showResults", methods=['POST'])
@@ -19,15 +19,41 @@ def showResults():
     #                               M A I N        C A L L
     #########################################################################################################################################################
     # send the text, if you want to train the model set second param to True, if you want to predict scores for the entered text set thirs param to True    #
-    sentenceVec = obj.mainproc(text, False, True)                                                                                                             #
+    sentenceVec = obj.mainproc(text, False, True)                                                                                                           #
     #########################################################################################################################################################
 
 
     sentenceCount = len(sentenceVec)
+    sentencelist = []
+    scoreList = []
+    i = 0
+    # converting the list of objects to a built in python parallel arrays so they can be JSONfied to be stored in the session (session in python is a JSON objects)
     for sent in sentenceVec:
         sent.pred = round(sent.pred, 2)
+        sentencelist.append(sent.text)
+        scoreList.append(sent.pred)
+        i+=1
+
+    session['sentenceList'] = sentencelist
+    session['scoreList'] =scoreList
     return render_template('results.html', transSentenceList = sentenceVec, sentenceCount= sentenceCount)
 
+
+@app.route("/sortResults", methods=['POST'])
+def sortResults():
+    sentenceList = session['sentenceList']
+    scoreList = session['scoreList']
+    sentenceVec = []
+    i = 0
+    for sent in sentenceList:
+        sentence = demoSentence(str(i),sent)
+        sentence.pred = scoreList[i]
+        sentenceVec.append(sentence)
+        i += 1
+
+    sentenceVec.sort(key=lambda sent: sent.pred, reverse=True)
+    sentenceCount = len(sentenceVec)
+    return render_template('results.html', transSentenceList=sentenceVec, sentenceCount=sentenceCount)
 
 
 # direct the user to the main page when the app starts
@@ -35,8 +61,11 @@ def showResults():
 def main():
 	return render_template('index.html')
 
+
+
 if __name__ == "__main__":
 	app.run()
+
 
 
 
