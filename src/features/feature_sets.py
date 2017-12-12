fb89835b61723be4c680d733f62fd0845507837a
Pepa Gencheva <pepa.k.gencheva@gmail.com>
2017-12-12 20:13:15 +0200

    Fix import

--- src/features/feature_sets.py
+++ src/features/feature_sets.py
@@ -1,7 +1,7 @@
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing.data import MinMaxScaler
 
-from features import alchemy_feat, counting_feat, \
+from src.features import alchemy_feat, counting_feat, \
     dict_feat, metadata_feat, nltk_feat, topics, knn_similarity, embeddings_feat
 
 from features.features import ToMatrix
