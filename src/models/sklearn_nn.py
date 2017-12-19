de136e1d83dfe84762ea6d67b7e6b4949a73d475
Pepa Gencheva <pepa.k.gencheva@gmail.com>
2017-12-20 00:22:51 +0200

    Last model configuration.

--- src/models/sklearn_nn.py
+++ src/models/sklearn_nn.py
@@ -6,14 +6,14 @@ from features.feature_sets import get_experimential_pipeline, get_cb_pipeline
 
 def run(test, train):
     # generate features
-    feats = get_experimential_pipeline(train)
+    feats = get_experimential_pipeline(train, test)
     train_x = feats.fit_transform(train)
     test_x = feats.fit_transform(test)
 
     # train
     train_y = [1 if sent.label > 0 else 0 for sent in train]
     clf = MLPClassifier(max_iter=100, solver='sgd', alpha=4, hidden_layer_sizes=(200, 50),
-                        random_state=1, activation='relu', learning_rate_init=0.03, batch_size=550)
+                        random_state=42, activation='relu', learning_rate_init=0.03, batch_size=550)
 
     clf.fit(train_x, train_y)
 
