1c46a704fc301f486fd20ea0805845eee823f96a
Pepa Gencheva <pepa.k.gencheva@gmail.com>
2017-12-24 01:01:49 +0200

    Implementation of a feature(sklearn style), reading serialized features.

--- src/features/features.py
+++ src/features/features.py
@@ -1,10 +1,26 @@
+import json
 from sklearn.pipeline import TransformerMixin
+from src.utils.config import get_config
 
 
 class Feature(TransformerMixin):
     """Feature Interface."""
     def fit(self, X, y=None, **fit_params):
         return self
+
+
+class ReadFeatures(Feature):
+    def __init__(self, feature_names):
+        config = get_config()
+        self._feature_dicts = {}
+        for feature in feature_names:
+            self._feature_dicts[feature] = json.loads(open(config['features_dump_dir']+feature).read())
+
+    def transform(self, X):
+        for _x in X:
+            for feature_name, feature_values in self._feature_dicts.items():
+                _x.features[feature_name] = feature_values[str(_x.id)+_x.debate.name]
+        return X
 
 
 class ToMatrix(Feature):
