58ab9ec0b16211c535210dbae6eb136cbe961f1f
Pepa Gencheva <pepa.k.gencheva@gmail.com>
2017-12-24 01:24:12 +0200

    Make use of the serialized pipeline and refactor.

--- src/models/sklearn_nn.py
+++ src/models/sklearn_nn.py
@@ -1,18 +1,20 @@
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing.data import MinMaxScaler
 
-from features.feature_sets import get_experimential_pipeline, get_cb_pipeline
+from features.feature_sets import get_cb_pipeline, get_serialized_pipeline, get_experimential_pipeline
 
 
 def run(test, train):
     # generate features
-    feats = get_experimential_pipeline(train, test)
-    train_x = feats.fit_transform(train)
-    test_x = feats.fit_transform(test)
 
+    # feats = get_experimential_pipeline(train)
+    feats = get_serialized_pipeline(train)
+
+    train_x = feats.fit_transform(train)
+    test_x = feats.transform(test)
     # train
-    train_y = [1 if sent.label > 0 else 0 for sent in train]
-    clf = MLPClassifier(max_iter=100, solver='sgd', alpha=4, hidden_layer_sizes=(200, 50),
+    train_y = [1 if sent.label_test > 0 else 0 for sent in train]
+    clf = MLPClassifier(max_iter=300, solver='sgd', alpha=4, hidden_layer_sizes=(200, 50),
                         random_state=42, activation='relu', learning_rate_init=0.03, batch_size=550)
 
     clf.fit(train_x, train_y)
