61cda3632ff777456b68530edf595a90546184c6
Pepa Gencheva <pepa.k.gencheva@gmail.com>
2018-08-26 20:45:54 +0300

    Create folder if doesn't exist.

--- src/features/save_features.py
+++ src/features/save_features.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 
 def get_claim_scores(sentences):
     return np.array([np.array([1 if s.label > 0 else 0] + [int(l) for l in s.labels]) for s in sentences])
@@ -19,6 +20,7 @@ def get_targets(debate_results, all_indices):
 
 
 def save_data(debate_sentences, crossvalidation_indices, getPipeline, name='crossval'):
+    os.makedirs(os.path.dirname('folds/'), exist_ok=True)
     debate_results = np.array([get_claim_scores(debate_sentence) for debate_sentence in debate_sentences])
     folds = []
     for i, indices in enumerate(crossvalidation_indices):
